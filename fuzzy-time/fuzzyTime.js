32814104b65cc8787732d8d9ef0ed385f8ef1658
Adam Harris <aharris88@users.noreply.github.com>
2015-05-16 09:44:26 -0600

    Add o'clock test

--- fuzzy-time/fuzzyTime.js
+++ fuzzy-time/fuzzyTime.js
@@ -1,5 +1,24 @@
-var fuzzyTime = function(time) {
-	
+var fuzzyTime = function(timeString) {
+	var time = getTimeObject(timeString);
+
+	function getTimeObject(timeString) {
+		var split = timeString.split(':');
+		return {
+			hour: split[0],
+			minute: split[0],
+			second: split[0]
+		};
+	}
+
+	function numberToString(number) {
+		var numbers = {
+			1: 'one',
+			2: 'two'
+		};
+		return numbers[number];
+	}
+
+	return numberToString(time.hour) + " o'clock";
 };
 
 module.exports = fuzzyTime;
--- fuzzy-time/tests.js
+++ fuzzy-time/tests.js
@@ -6,4 +6,11 @@ describe("fuzzyTime", function() {
 	it("should exist", function() {
 		expect(fuzzyTime).to.exist;
 	});
+	it("should return a string", function() {
+		expect(fuzzyTime('')).to.be.a('string');
+	});
+	it("should return o'clock", function() {
+		expect(fuzzyTime('1:00')).to.equal("one o'clock");
+		expect(fuzzyTime('2:00')).to.equal("two o'clock");
+	});
 });
